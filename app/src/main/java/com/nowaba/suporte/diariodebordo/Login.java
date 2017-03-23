d9f0fecb239c105fb437ab5f61905d1a91df4133
jonathanBAugusto <jonathanbersot21@gmail.com>
2017-03-23 10:36:24 -0300

    teste

--- app/src/main/java/com/nowaba/suporte/diariodebordo/Login.java
+++ app/src/main/java/com/nowaba/suporte/diariodebordo/Login.java
@@ -1,13 +1,37 @@
 package com.nowaba.suporte.diariodebordo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+
+    private Button btNovo;
+    private Button btEntrar;
+    private EditText etUser;
+    private EditText etSenha;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btNovo = (Button) findViewById(R.id.btNew);
+        btEntrar = (Button) findViewById(R.id.btEnter);
+        etUser = (EditText) findViewById(R.id.etLogin);
+        etSenha = (EditText) findViewById(R.id.etSenha);
+
+        btNovo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent novoCad= new Intent();
+            }
+        });
+
+
     }
 }
